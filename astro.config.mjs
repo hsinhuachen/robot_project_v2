73c7f1d290b385e31d8db4784e8b489ab9fd58b2
kevin chen <yha0971@gmail.com>
2026-01-02 18:53:03 +0800

    feat: setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    base: '/',
+    //只要這行就可以把 root path 設定在
+    outDir: '../public/',
+    site: 'https://eray.4pt.tw/',
+    build: {
+        format: 'file'
+    },
+    integrations: [sitemap(), react()]
+});
